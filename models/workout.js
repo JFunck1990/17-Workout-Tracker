69bf9fd2ee243d91b2fd1671e3de8443efb97861
Jack Funck <jackfunck21@gmail.com>
2021-04-03 10:05:00 -0400

    working on schema

--- models/workout.js
+++ models/workout.js
@@ -3,8 +3,34 @@ const mongoose = require("mongoose");
 const Schema = mongoos.Schema;
 
 const workoutSchema = new Schema({
+    exercises: [
+    {
+        type: {
+            type: String
+        },
 
-});
+        name: {
+            type: String
+        },
+
+        duration: {
+            type: Number
+        },
+
+        weight: {
+            type: Number
+        },
+
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        }
+
+    }
+    ]
+    });
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
